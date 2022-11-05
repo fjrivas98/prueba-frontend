7b85027aee9ccb84714eb58eb9d4e21cc5b6820c
fjrivas98 <franciscojavierrivasbascon@gmail.com>
2022-11-05 12:34:29 +0100

    Añadido: - function icreasedMeasurements(total de días que ha habido un incremento respecto al anterior)

--- 3.js
+++ 3.js
@@ -4,6 +4,13 @@
 // Ejemplo: en el caso de [1, 5, 3, 5] existen 2 incremetos ( el día 2 respecto al dia 1 y el dia 4 respecto al día 3 )
 
 function increasedMeasurements(measurements) {
+    let incrementos = 0;
+    measurements.forEach((value, index) =>{
+        if(value > measurements[index-1]){
+            incrementos++;
+        }
+    })
+    return incrementos;
 }
 
 /**
